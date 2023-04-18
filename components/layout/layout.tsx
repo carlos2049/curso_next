c533cb537dc7ac0621c6f80db57503521c5e33b0
carlos2049 <carlos.20499@gmail.com>
2023-04-17 23:25:28 -0400

    add css

--- components/layout/layout.tsx
+++ components/layout/layout.tsx
@@ -1,18 +1,29 @@
 import React from 'react'
 import Navbar from '@components/Navbar/Navbar'
+// import style from './layout.module.css'
 
 type Props = {
     children: React.ReactNode
 }
 
+
 const Layout : React.FC<Props> =({children}: any)=> {
 
   return(
-		<div>
-            <Navbar />
-            {children}
-            <footer>fotter</footer>
-        </div>
+		// <div className={style.container}>
+    <div className='container'> 
+      <Navbar />
+        {children}
+      <footer className='container'>fotter</footer>
+      <style jsx>
+        {`
+          .container {
+            background : blue;
+          }  
+          
+        `}
+      </style>
+    </div>
   )
 }
 export default Layout
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import {AppProps} from 'next/app'
 import Layout from 'components/layout/layout'
+// import '../style.css'
 
 function MyApp({Component, pageProps}: AppProps){
     return (
